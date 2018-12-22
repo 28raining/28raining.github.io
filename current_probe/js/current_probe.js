16586107efce262dfd98e3287e587ea79ed6a783
willzglue <will@zglue.com>
2018-12-21 19:13:30 -0800

    removed ble added usb

--- current_probe/js/current_probe.js
+++ current_probe/js/current_probe.js
@@ -23,29 +23,34 @@ function rand() {
 
   var device;
 
-navigator.usb.requestDevice({ filters: [{ vendorId: 0x2341 }] })
-.then(selectedDevice => {
-   device = selectedDevice;
-   return device.open(); // Begin a session.
- })
-.then(() => device.selectConfiguration(1)) // Select configuration #1 for the device.
-.then(() => device.claimInterface(2)) // Request exclusive control over interface #2.
-.then(() => device.controlTransferOut({
-    requestType: 'class',
-    recipient: 'interface',
-    request: 0x22,
-    value: 0x01,
-    index: 0x02})) // Ready to receive data
-.then(() => device.transferIn(5, 64)) // Waiting for 64 bytes of data from endpoint #5.
-.then(result => {
-  let decoder = new TextDecoder();
-  console.log('Received: ' + decoder.decode(result.data));
-})
-.catch(error => { console.log(error); });
+document.getElementById("ble_button").addEventListener('pointerup', function(event) {
+    navigator.usb.requestDevice({ filters: [{ vendorId: 0x2341 }] })
+    .then(selectedDevice => {
+    device = selectedDevice;
+    return device.open(); // Begin a session.
+    })
+    .then(() => device.selectConfiguration(1)) // Select configuration #1 for the device.
+    .then(() => device.claimInterface(2)) // Request exclusive control over interface #2.
+    .then(() => device.controlTransferOut({
+        requestType: 'class',
+        recipient: 'interface',
+        request: 0x22,
+        value: 0x01,
+        index: 0x02})) // Ready to receive data
+    .then(() => device.transferIn(5, 64)) // Waiting for 64 bytes of data from endpoint #5.
+    .then(result => {
+    let decoder = new TextDecoder();
+    console.log('Received: ' + decoder.decode(result.data));
+    })
+    .catch(error => { console.log(error); });
+});
 
-navigator.bluetooth.requestDevice({
-    acceptAllDevices: true,
-    optionalServices: ['battery_service']
-  })
-  .then(device => {console.log(device.name);})
-  .catch(error => { console.log(error); });
+
+// document.getElementById("ble_button").addEventListener('pointerup', function(event) {
+//     navigator.bluetooth.requestDevice({
+//         acceptAllDevices: true,
+//         optionalServices: ['battery_service']
+//         })
+//         .then(device => {console.log(device.name);})
+//         .catch(error => { console.log(error); });
+// });
